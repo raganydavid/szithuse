3574c9ccd673a0138d94acf2e77344e39ca9405a
Ragany David <david.ragany@gmail.com>
2023-05-25 18:37:17 +0200

    Init

---
+++ test/index.js
@@ -0,0 +1,10 @@
+const { Builder, By, Key } = require('selenium-webdriver');
+require('chromedriver');
+const url = 'https://szit.hu';
+
+function szitTeszt() {
+    let driver = new Builder().forBrowser('chrome').build();
+    driver.get(url);
+    driver.findElement(By.id('qsearch__in')).sendKeys('recept', Key.ENTER);
+}
+szitTeszt();
